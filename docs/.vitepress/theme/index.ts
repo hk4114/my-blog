3319c0e2bfbf693e929c4625db65f6d77d38dc7d
hk4114 <741180788@qq.com>
2024-06-23 16:15:07 +0800

    docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,32 @@
-import BlogTheme from '@sugarat/theme'
-
+import BlogTheme from "@sugarat/theme";
 // 自定义样式重载
-// import './style.scss'
-
+import "./style.scss";
 // 自定义主题色
-// import './user-theme.css'
+import "./user-theme.css";
 
-export default BlogTheme
+import MNavLinks from "./components/MNavLinks.vue";
+
+// 导航
+import { h } from "vue";
+import { useData } from "vitepress";
+
+export default {
+  extends: BlogTheme,
+  enhanceApp({ app }) {
+    // 注册组件
+    app.component("MNavLinks", MNavLinks);
+  },
+  Layout: () => {
+    const props: Record<string, any> = {};
+    // 获取 frontmatter
+    const { frontmatter } = useData();
+
+    /* 添加自定义 class */
+    if (frontmatter.value?.layoutClass) {
+      props.class = frontmatter.value.layoutClass;
+    }
+
+    //@ts-ignore
+    return h(BlogTheme.Layout, props);
+  },
+};
